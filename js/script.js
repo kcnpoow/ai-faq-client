c1c9e39b005d1b56462fdbca30b00b5edd6c653a
Akhmetov Sabyrjan <116171446+kcnpoow@users.noreply.github.com>
2024-11-28 01:33:51 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,21 +2,27 @@ const btnOpenChat = document.getElementById('btnOpenChat');
 const chat = document.getElementById('chat');
 const chatBody = document.getElementById('chatBody');
 const chatInput = document.getElementById('chatInput');
+const backdrop = document.getElementById('backdrop');
 
-btnOpenChat.addEventListener("click", () => {
+backdrop.addEventListener("click", () => {
+    chat.classList.remove('opened');
+    backdrop.classList.remove('shown');
+});
+
+btnOpenChat.addEventListener("click", (e) => {
     chat.classList.toggle('opened');
+    backdrop.classList.toggle('shown');
 });
 
 chatInput.addEventListener("keydown", async (e) => {
     if (e.key == "Enter" && chatInput.value != "") {
-        const userMessage = chatInput.value;
-        await sendMessage(userMessage);
+        const userQuestion = chatInput.value;
         chatInput.value = "";
+        await sendQuestion(userQuestion);
     }
 });
 
-
-async function sendMessage(message) {
+async function sendQuestion(message) {
     const senderMessage = document.createElement("div");
     senderMessage.classList.add("chat__message", "chat__message_sender");
     senderMessage.textContent = message;
@@ -24,7 +30,14 @@ async function sendMessage(message) {
     chatBody.appendChild(senderMessage);
     chatBody.scrollTo(0, chatBody.scrollHeight);
 
+    chatInput.disabled = true;
     try {
+        const receiverMessage = document.createElement("div");
+        receiverMessage.classList.add("chat__message", "chat__message_receiver");
+        receiverMessage.textContent = 'Loading...';
+        chatBody.appendChild(receiverMessage);
+        chatBody.scrollTo(0, chatBody.scrollHeight);
+
         const response = await fetch('https://ai-faq-server.onrender.com/question', {
             method: 'POST',
             headers: {
@@ -34,25 +47,13 @@ async function sendMessage(message) {
             body: JSON.stringify({ question: message })
         });
 
-
         const data = await response.json();
-        receiveMessage(data.response);
 
-        return data;
+        receiverMessage.textContent = data.response;
+        chatBody.scrollTo(0, chatBody.scrollHeight);
     } catch (error) {
         console.log(error);
+    } finally {
+        chatInput.disabled = false;
     }
-
-    return null;
 }
-
-function receiveMessage(answer) {
-    const receiverMessage = document.createElement("div");
-    receiverMessage.classList.add("chat__message", "chat__message_receiver");
-    receiverMessage.textContent = answer;
-
-    chatBody.appendChild(receiverMessage);
-    chatBody.scrollTo(0, chatBody.scrollHeight);
-}
-
-
